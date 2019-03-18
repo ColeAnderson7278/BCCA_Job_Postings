c01c77d1ab123fba5151b434bf302f937e9412cc
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2019-03-18 14:20:50 -0500

    Updated date format for home pages.
    I added in some JS that took in my dates
    and return them in a format I found more
    suitable.

--- src/main/resources/static/js/home_page.js
+++ src/main/resources/static/js/home_page.js
@@ -35,3 +35,44 @@ function testAllImages() {
 }
 
 testAllImages();
+
+function setAllDates() {
+    const dateHolders = document.querySelectorAll(".postedDate");
+    for (var holder of dateHolders) {
+        var date = holder.innerText.split("-");
+        holder.innerText =
+            "Posted: " + setToMonth(date[1]) + " " + date[2] + ", " + date[0];
+    }
+}
+
+setAllDates();
+
+function setToMonth(num) {
+    if (num == "01") {
+        return "Jan";
+    } else if (num == "02") {
+        return "Feb";
+    } else if (num == "03") {
+        return "Mar";
+    } else if (num == "04") {
+        return "Apr";
+    } else if (num == "05") {
+        return "May";
+    } else if (num == "06") {
+        return "Jun";
+    } else if (num == "07") {
+        return "Jul";
+    } else if (num == "08") {
+        return "Aug";
+    } else if (num == "09") {
+        return "Sep";
+    } else if (num == "10") {
+        return "Oct";
+    } else if (num == "11") {
+        return "Nov";
+    } else if (num == "12") {
+        return "Dec";
+    } else {
+        return "Error";
+    }
+}
